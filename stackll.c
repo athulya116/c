6547458f08be37e32fc67e8372f6d4c7fb788b74
athulya116 <116333075+athulya116@users.noreply.github.com>
2023-02-19 20:08:50 +0530

    Add files via upload

--- stackll.c
+++ stackll.c
@@ -1,115 +1,78 @@
 #include<stdio.h>
 #include<stdlib.h>
-void push();
-void pop();
-void display();
 struct node
 {
-int val;
-struct node *next;
+    int data;
+    struct node *next;
 };
-struct node *head;
-void main()
-{
-int choice=0;
-printf("\n stack operations using linked list\n");
-
-while(choice!=4)
-{
-printf("\n\nchoose an operation given below\n");
-printf("1. PUSH \n 2. POP \n 3. DISPLAY  \n 4.EXIT ");
-printf("Enter your choice\n");
-scanf("%d",&choice);
-switch(choice)
-{
-case 1:
-{
-push();
-break;
-}
-case 2:
-{
-pop();
-break;
-}
-case 3:
-{
-display();
-break;
-}
-
-case 4:
-{
-printf("exiting");
-break;
-}
-default:
-{
-printf("please enter a valid choice");
-}
-};
-}
-}
+struct node *top,*temp;
 void push()
 {
-int val;
-struct node *ptr=(struct node*)malloc(sizeof(struct node));
-if(ptr==NULL)
-{
-printf("not able to push element");
-}
-else
-{
-printf("enter the value");
-scanf("%d",&val);
-if(head==NULL)
-{
-ptr->val=val;
-ptr->next=NULL;
-head=ptr;
-}
-else
-{
-ptr->val=val;
-ptr->next=head;
-head=ptr;
-}
-printf("item pushed");
-}
+  int item;
+  struct node *newnode;
+  newnode=(struct node *)malloc(sizeof(struct node));
+  printf("enter item");
+  scanf("%d",&item);
+  if(top==NULL)
+  {
+    newnode->data=item;
+    newnode->next=NULL;
+    top=newnode;
+  }
+  else
+  {
+    newnode->data=item;
+    newnode->next=top;
+    top=newnode;
+  }
 }
 void pop()
-{
-int item;
-struct node *ptr;
-if (head==NULL)
-{
-printf("Underflow");
-}
-else
-{
-item=head->val;
-ptr=head;
-head=head->next;
-free(ptr);
-printf("item popped ");
-}
+{ 
+  temp=top;
+  if(top==NULL)
+  {
+    printf("stack underflow");
+  }
+  else{
+     top=top->next;
+     free(temp);
+  }
 }
 void display()
 {
-int i;
-struct node *ptr;
-ptr=head;
-if(ptr==NULL)
-{
-printf("Stack is empty");
+  temp=top;
+  if(top==NULL)
+  {
+    printf("stack empty");
+  }
+  else{
+    while(temp!=NULL)
+    {
+        printf("%d\n",temp->data);
+        temp=temp->next;
+    }
+  }
 }
-else
-{
-printf("printing stack elemnts\n");
-while(ptr!=NULL)
-{
-printf("%d\n",ptr->val);
-ptr=ptr->next;
-}
-}
+void main()
+{   int choice;
+    printf ("the stack operations are\n");
+    printf(" 1.push  2.pop   3.display  4.exit");
+    do
+    {
+        printf("enter your choice");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:push();
+            break;
+            case 2:pop();
+            break;
+            case 3:display();
+            break;
+            case 4:exit(0);
+            break;
+            default:printf("enter a valid option");
+        }
+    }
+    while(choice!=4);
 }
